16850acaf22a9c820ad9629aab18f34b2fc96008
MoonMonsters <qxinhai@yeah.net>
2018-05-24 23:16:09 +0800

    1.修复用户短时间内多次重复点赞数据显示错误问题 2.添加网站图标，一只熊猫 3.修改某些字符串显示

--- apps/like/views.py
+++ apps/like/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.contrib import auth
+import threading
+
 from django.http import JsonResponse
 
 from .models import BlogLike
 from blog.models import Blog
+
+lock = threading.Lock()
 
 
 def give_blog_a_like(request):
@@ -17,6 +19,9 @@ def give_blog_a_like(request):
 		blog_id = request.GET.get('blog_id', -1)
 		data['status'] = 'SUCCESS'
 		blog_like = BlogLike.objects.filter(blog_id=blog_id, user=request.user)
+		# 将此部分代码锁住，避免在同一个人在多时间重复多次点击时，出现加了很多次或者出现负数的情况
+		# 虽然点击完成后，数据还是正常的
+		lock.acquire()
 		# 如果该用户已对该博客点过赞，则取消赞
 		if blog_like.exists():
 			data['type'] = 'unlike'
@@ -26,8 +31,9 @@ def give_blog_a_like(request):
 			data['type'] = 'like'
 			# 保存数据
 			BlogLike.objects.create(blog=Blog.objects.filter(pk=blog_id).first(), user=request.user)
+		lock.release()
 	else:
 		data['status'] = 'ERROR'
-		data['description'] = '未登录，不可点赞'
+		data['description'] = '请先登录再点赞'
 
 	return JsonResponse(data)
